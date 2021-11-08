faf62d221c3978fe54723e82ee05103332cef670
joaodematejr <joaodematejr@gmail.com>
2021-11-08 16:03:50 -0300

    att

--- sicredi/app/routes.php
+++ sicredi/app/routes.php
@@ -28,17 +28,54 @@ return function (App $app) {
     });
 
     //IMPLEMENTAÇÃO DO JOÃO
-    $app->get('/exemplo', function (Request $request, Response $response) {
+    $app->get('/exemploJson', function (Request $request, Response $response) {
         $data = ['name' => 'Bob 01', 'age' => 40];
         $payload = json_encode($data);
         $response->getBody()->write($payload);
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
     });
 
-    $app->delete('/exemplo', function (Request $request, Response $response) {
-        $data = ['mensagem' => 'Deletado com Sucesso!'];
-        $payload = json_encode($data);
+    $app->get('/cep', function (Request $request, Response $response) {
+        $cep = '88050000';
+        $link = "https://viacep.com.br/ws/$cep/json/";
+        $ch = curl_init($link);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $responseAPI = curl_exec($ch);
+        curl_close($ch);
+        $response->getBody()->write($responseAPI);
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
+    });
+
+    $app->get('/basic', function (Request $request, Response $response) {
+        $body = '<SOAP-ENV:Envelope ...>...</SOAP-ENV:Envelope>';
+        $ch = curl_init('https://test.ipg-online.com/ipgapi/services');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: text/xml']);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, 'WS101._.007:myPW');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+        $payload = json_encode($ch);
         $response->getBody()->write($payload);
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+        return $response->withHeader('Content-Type', 'text/xml')->withStatus(200);
+    });
+
+    $app->get('/phpCurl', function (Request $request, Response $response) {
+        // telling cURL to verify the server certificate:
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        // setting the path where cURL can find the certificate to verify the
+        // received server certificate against:
+        curl_setopt($ch, CURLOPT_CAINFO, '/Users/joaodematejr/Documents/Github/PhpSicredi/sicredi/keys/WS2724189910._.1.pem');
+        curl_setopt($ch, CURLOPT_SSLCERT, "/Users/joaodematejr/Documents/Github/PhpSicredi/sicredi/keys/WS2724189910._.1.pem");
+        // setting the path where cURL can find the client certificate’s
+        // private key:    
+        curl_setopt($ch, CURLOPT_SSLKEY, "/Users/joaodematejr/Documents/Github/PhpSicredi/sicredi/keys/WS2724189910._.1.key");
+        // setting the key password:
+        curl_setopt($ch, CURLOPT_SSLKEYPASSWD, "ckp_1193927132");
+
     });
 };
