6b34a9bc563b72c9d63a2a0aa6929c0611b58009
João Dematé <joaodematejr@gmail.com>
2021-11-16 09:14:26 -0300

    Implementação debit

--- sicredi/app/routes.php
+++ sicredi/app/routes.php
@@ -9,6 +9,7 @@ use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
+    #GRUPO DE ROTAS
     $app->options('/{routes:.*}', function (
         Request $request,
         Response $response
@@ -17,21 +18,19 @@ return function (App $app) {
         return $response;
     });
 
+    #BARRA PARA OLA MUNDO
     $app->get('/', function (Request $request, Response $response) {
         $response->getBody()->write('Hello world!');
         return $response;
     });
 
+    #LISTA DE USUARIOS 
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
     });
-
-    function get_call_api()
-    {
-      return true;
-    }
     
+    #CANCELAMENTO NO MESMO DIA 
     $app->post('/cancelSameDay', function (Request $request, Response $response) { 
         $data = $request->getParsedBody();
         if (empty($data['user'])) {
@@ -50,20 +49,20 @@ return function (App $app) {
             try {
                 $xml  = '<?xml version="1.0" encoding="UTF-8"?>';
                 $xml .= '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">';
-                    $xml .= '<SOAP-ENV:Header />';
-                        $xml .= '<SOAP-ENV:Body>';
-                            $xml .= '<ipgapi:IPGApiOrderRequest xmlns:v1="http://ipg-online.com/ipgapi/schemas/v1" xmlns:ipgapi="http://ipg-online.com/ipgapi/schemas/ipgapi">';
-                                $xml .= '<v1:Transaction>';
-                                    $xml .= '<v1:CreditCardTxType>';
-                                        $xml .= '<v1:Type>void</v1:Type>';
-                                    $xml .= '</v1:CreditCardTxType>';
-                                    $xml .= '<v1:TransactionDetails>';
-                                        $xml .= '<v1:OrderId>'.$data['orderId'].'</v1:OrderId>';
-                                        $xml .= '<v1:TDate>'.$data['tDate'].'</v1:TDate>';
-                                    $xml .= '</v1:TransactionDetails>';
-                                $xml .= '</v1:Transaction>';
-                            $xml .= '</ipgapi:IPGApiOrderRequest>';  
-                        $xml .= '</SOAP-ENV:Body>';
+                $xml .= '<SOAP-ENV:Header />';
+                $xml .= '<SOAP-ENV:Body>';
+                $xml .= '<ipgapi:IPGApiOrderRequest xmlns:v1="http://ipg-online.com/ipgapi/schemas/v1" xmlns:ipgapi="http://ipg-online.com/ipgapi/schemas/ipgapi">';
+                $xml .= '<v1:Transaction>';
+                $xml .= '<v1:CreditCardTxType>';
+                $xml .= '<v1:Type>void</v1:Type>';
+                $xml .= '</v1:CreditCardTxType>';
+                $xml .= '<v1:TransactionDetails>';
+                $xml .= '<v1:OrderId>'.$data['orderId'].'</v1:OrderId>';
+                $xml .= '<v1:TDate>'.$data['tDate'].'</v1:TDate>';
+                $xml .= '</v1:TransactionDetails>';
+                $xml .= '</v1:Transaction>';
+                $xml .= '</ipgapi:IPGApiOrderRequest>';  
+                $xml .= '</SOAP-ENV:Body>';
                 $xml .= '</SOAP-ENV:Envelope>';
 
                 $ch = curl_init("https://test.ipg-online.com/ipgapi/services");
@@ -106,6 +105,7 @@ return function (App $app) {
         }
     });
 
+    #CANCELAR PAGAMENTO
     $app->post('/cancel', function (Request $request, Response $response) { 
         $data = $request->getParsedBody();
         if (empty($data['user'])) {
@@ -130,23 +130,23 @@ return function (App $app) {
             try {
                 $xml  = '<?xml version="1.0" encoding="UTF-8"?>';
                 $xml .= '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">';
-                    $xml .= '<SOAP-ENV:Header />';
-                        $xml .= '<SOAP-ENV:Body>';
-                            $xml .= '<ipgapi:IPGApiOrderRequest xmlns:v1="http://ipg-online.com/ipgapi/schemas/v1" xmlns:ipgapi="http://ipg-online.com/ipgapi/schemas/ipgapi">';
-                                $xml .= '<v1:Transaction>';
-                                    $xml .= '<v1:CreditCardTxType>';
-                                        $xml .= '<v1:Type>credit</v1:Type>';
-                                    $xml .= '</v1:CreditCardTxType>';
-                                    $xml .= '<v1:Payment>';
-                                        $xml .= '<v1:ChargeTotal>'.$data['chargeTotal'].'</v1:ChargeTotal>';
-                                        $xml .= '<v1:Currency>'.$data['currency'].'</v1:Currency>';
-                                    $xml .= '</v1:Payment>';
-                                    $xml .= '<v1:TransactionDetails>';
-                                        $xml .= '<v1:OrderId>'.$data['orderId'].'</v1:OrderId>';
-                                    $xml .= '</v1:TransactionDetails>';
-                                $xml .= '</v1:Transaction>';
-                            $xml .= '</ipgapi:IPGApiOrderRequest>';
-                        $xml .= '</SOAP-ENV:Body>';
+                $xml .= '<SOAP-ENV:Header />';
+                $xml .= '<SOAP-ENV:Body>';
+                $xml .= '<ipgapi:IPGApiOrderRequest xmlns:v1="http://ipg-online.com/ipgapi/schemas/v1" xmlns:ipgapi="http://ipg-online.com/ipgapi/schemas/ipgapi">';
+                $xml .= '<v1:Transaction>';
+                $xml .= '<v1:CreditCardTxType>';
+                $xml .= '<v1:Type>credit</v1:Type>';
+                $xml .= '</v1:CreditCardTxType>';
+                $xml .= '<v1:Payment>';
+                $xml .= '<v1:ChargeTotal>'.$data['chargeTotal'].'</v1:ChargeTotal>';
+                $xml .= '<v1:Currency>'.$data['currency'].'</v1:Currency>';
+                $xml .= '</v1:Payment>';
+                $xml .= '<v1:TransactionDetails>';
+                $xml .= '<v1:OrderId>'.$data['orderId'].'</v1:OrderId>';
+                $xml .= '</v1:TransactionDetails>';
+                $xml .= '</v1:Transaction>';
+                $xml .= '</ipgapi:IPGApiOrderRequest>';
+                $xml .= '</SOAP-ENV:Body>';
                 $xml .= '</SOAP-ENV:Envelope>';
                 $ch = curl_init("https://test.ipg-online.com/ipgapi/services");
                 curl_setopt($ch, CURLOPT_POST, TRUE); 
@@ -189,7 +189,7 @@ return function (App $app) {
         }
     });
 
-
+    #PAGAMENTO VIA CREDITO
     $app->post('/credit', function (Request $request, Response $response) {
         $data = $request->getParsedBody();
         if (empty($data['user'])) {
@@ -219,28 +219,28 @@ return function (App $app) {
         } else {
             try {
                 $xml  = '<?xml version="1.0" encoding="UTF-8"?>';
-                    $xml .= '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">';
-                        $xml .= '<SOAP-ENV:Header />';
-                        $xml .= '<SOAP-ENV:Body>';
-                            $xml .= '<ipgapi:IPGApiOrderRequest xmlns:v1="http://ipg-online.com/ipgapi/schemas/v1" xmlns:ipgapi="http://ipg-online.com/ipgapi/schemas/ipgapi">';
-                                $xml .= '<v1:Transaction>';
-                                    $xml .= '<v1:CreditCardTxType>';
-                                        $xml .= '<v1:Type>credit</v1:Type>';
-                                    $xml .= '</v1:CreditCardTxType>';
-                                    $xml.='<v1:CreditCardData>
-                                                <v1:CardNumber>'.$data['cardNumber'].'</v1:CardNumber>
-                                                <v1:ExpMonth>'.$data['expMonth'].'</v1:ExpMonth>
-                                                <v1:ExpYear>'.$data['expYear'].'</v1:ExpYear>
-                                            </v1:CreditCardData>';
-                                    $xml .= '<v1:cardFunction>credit</v1:cardFunction>';
-                                    $xml .= '<v1:Payment>';
-                                        $xml .= '<v1:ChargeTotal>'.$data['chargeTotal'].'</v1:ChargeTotal>';
-                                        $xml .= '<v1:Currency>'.$data['currency'].'</v1:Currency>';
-                                    $xml .= '</v1:Payment>';
-                                $xml .= '</v1:Transaction>';
-                            $xml .= '</ipgapi:IPGApiOrderRequest>';
-                        $xml .= '</SOAP-ENV:Body>';
-                    $xml .= '</SOAP-ENV:Envelope>';
+                $xml .= '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">';
+                $xml .= '<SOAP-ENV:Header />';
+                $xml .= '<SOAP-ENV:Body>';
+                $xml .= '<ipgapi:IPGApiOrderRequest xmlns:v1="http://ipg-online.com/ipgapi/schemas/v1" xmlns:ipgapi="http://ipg-online.com/ipgapi/schemas/ipgapi">';
+                $xml .= '<v1:Transaction>';
+                $xml .= '<v1:CreditCardTxType>';
+                $xml .= '<v1:Type>credit</v1:Type>';
+                $xml .= '</v1:CreditCardTxType>';
+                $xml.='<v1:CreditCardData>
+                <v1:CardNumber>'.$data['cardNumber'].'</v1:CardNumber>
+                <v1:ExpMonth>'.$data['expMonth'].'</v1:ExpMonth>
+                <v1:ExpYear>'.$data['expYear'].'</v1:ExpYear>
+                </v1:CreditCardData>';
+                $xml .= '<v1:cardFunction>credit</v1:cardFunction>';
+                $xml .= '<v1:Payment>';
+                $xml .= '<v1:ChargeTotal>'.$data['chargeTotal'].'</v1:ChargeTotal>';
+                $xml .= '<v1:Currency>'.$data['currency'].'</v1:Currency>';
+                $xml .= '</v1:Payment>';
+                $xml .= '</v1:Transaction>';
+                $xml .= '</ipgapi:IPGApiOrderRequest>';
+                $xml .= '</SOAP-ENV:Body>';
+                $xml .= '</SOAP-ENV:Envelope>';
                 $ch = curl_init("https://test.ipg-online.com/ipgapi/services");
                 curl_setopt($ch, CURLOPT_POST, TRUE); 
                 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
@@ -279,7 +279,126 @@ return function (App $app) {
                 $response->getBody()->write($th);
                 return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
             }
-            
         }
     });
+
+    #PAGAMENTO VIA DEBITO
+    $app->post('/debit', function (Request $request, Response $response) {
+        $data = $request->getParsedBody();
+        if (empty($data['user'])) {
+            $response->getBody()->write(json_encode(['message' => 'Você precisa preencher User']));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
+        } elseif (empty($data['pass'])) {
+            $response->getBody()->write(json_encode(['message' => 'Você precisa preencher Pass']));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
+        } elseif (empty($data['certPass'])) {
+            $response->getBody()->write(json_encode(['message' => 'Você precisa preencher CertPass']));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
+        } elseif (empty($data['cardNumber'])) {
+            $response->getBody()->write(json_encode(['message' => 'Você precisa preencher CardNumber']));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
+        } elseif (empty($data['expMonth'])) {
+            $response->getBody()->write(json_encode(['message' => 'Você precisa preencher ExpMonth']));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
+        } elseif (empty($data['expYear'])) {
+            $response->getBody()->write(json_encode(['message' => 'Você precisa preencher ExpYear']));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
+        } elseif (empty($data['chargeTotal'])) {
+            $response->getBody()->write(json_encode(['message' => 'Você precisa preencher ChargeTotal']));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
+        } elseif (empty($data['currency'])) {
+            $response->getBody()->write(json_encode(['message' => 'Você precisa preencher Currency']));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
+        } else {
+            try {
+                $xml  = '<?xml version="1.0" encoding="UTF-8"?>';
+                $xml .= '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">';
+                $xml .= '<SOAP-ENV:Header />';
+                $xml .= '<SOAP-ENV:Body>';
+                $xml .= '<ipgapi:IPGApiOrderRequest xmlns:v1="http://ipg-online.com/ipgapi/schemas/v1" xmlns:ipgapi="http://ipg-online.com/ipgapi/schemas/ipgapi">';
+                $xml .= '<v1:Transaction>';
+                $xml .= '<v1:CreditCardTxType>';
+                $xml .= '<v1:Type>sale</v1:Type>';
+                $xml .= '</v1:CreditCardTxType>';
+                $xml.='<v1:CreditCardData>
+                <v1:CardNumber>'.$data['cardNumber'].'</v1:CardNumber>
+                <v1:ExpMonth>'.$data['expMonth'].'</v1:ExpMonth>
+                <v1:ExpYear>'.$data['expYear'].'</v1:ExpYear>
+                </v1:CreditCardData>';
+                $xml .= '<v1:cardFunction>debit</v1:cardFunction>';
+                $xml .= '<v1:Payment>';
+                $xml .= '<v1:ChargeTotal>'.$data['chargeTotal'].'</v1:ChargeTotal>';
+                $xml .= '<v1:Currency>'.$data['currency'].'</v1:Currency>';
+                $xml .= '</v1:Payment>';
+                $xml .= '</v1:Transaction>';
+                $xml .= '</ipgapi:IPGApiOrderRequest>';
+                $xml .= '</SOAP-ENV:Body>';
+                $xml .= '</SOAP-ENV:Envelope>';
+                $ch = curl_init("https://test.ipg-online.com/ipgapi/services");
+                curl_setopt($ch, CURLOPT_POST, TRUE); 
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
+                curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+                //ENVIADO NO JSON DA API
+                curl_setopt($ch, CURLOPT_USERPWD, "{$data['user']}:{$data['pass']}"); 
+                //curl_setopt($ch, CURLOPT_USERPWD, "WS2724189910._.1:vR[7B,6eBV");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+
+                //MODO PRODUÇÃO MUDAR PARA TRUE
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+                curl_setopt($ch, CURLOPT_CAINFO, "C:\certs\geotrust.crt");
+                curl_setopt($ch, CURLOPT_SSLCERT, "C:\certs\WS2724189910._.1.pem");
+                curl_setopt($ch, CURLOPT_SSLKEY, "C:\certs\WS2724189910._.1.key");
+
+                curl_setopt($ch, CURLOPT_SSLKEYPASSWD, $data['certPass']);
+                //curl_setopt($ch, CURLOPT_SSLKEYPASSWD, "j@9EGB4]Bh");
+
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                $result = curl_exec($ch);
+                $errors = curl_error($ch); 
+                curl_close($ch);
+
+                if ($result === FALSE) {
+                    $payload = json_encode($errors);
+                    $response->getBody()->write($payload);
+                    return $response->withHeader('Content-Type', 'application/json')->withStatus(405);
+                } else {
+                    $payload = json_encode($result);
+                    $response->getBody()->write($result);
+                    return $response->withHeader('Content-Type', 'application/xml')->withStatus(200);
+                }
+            } catch (\Throwable $th) {
+                $response->getBody()->write($th);
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+            }
+        }
+    });
+
+    #PAGAMENTO VIA PIX
+    $app->post('/pix', function (Request $request, Response $response) { 
+        $response->getBody()->write(json_encode(['message' => 'Pagamento via PIX']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+    });
+
+    #PAGAMENTO VIA BOLETO
+    $app->post('/boleto', function (Request $request, Response $response) { 
+        $data = $request->getParsedBody();
+        if (!empty($data)) {
+            if (empty($data['user'])) { 
+                $response->getBody()->write(json_encode(['message' => 'Faltando parametros para boleto']));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
+            }else{
+                $response->getBody()->write(json_encode(['message' => 'Pagamento via Boleto']));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+            }
+        } else {
+            $response->getBody()->write(json_encode($data));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+        }
+    });
+
+
+
+
 };
